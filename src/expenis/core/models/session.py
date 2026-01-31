67cec9e6ac2398e7b8e0a6b63f97c9b16b0f5683
George Grishchenkov <g.grishchenkov@hh.ru>
2026-01-31 13:57:09 +0300

    feat: add Session model with basic fields and constraints

---
+++ src/expenis/core/models/session.py
@@ -0,0 +1,19 @@
+from datetime import UTC, datetime
+
+from peewee import AutoField, Check, DateTimeField, FloatField, ForeignKeyField, IntegerField, Model, TextField
+
+from .account import Account
+from .category import Category
+from .database import db
+
+
+class Session(Model):
+    id = TextField(primary_key=True)
+    user_id = IntegerField(null=True)
+    status = TextField(null=False, constraints=[Check("type IN ('pending', 'confirmed')")])
+    created_at = DateTimeField(null=False, default=lambda: datetime.now(UTC))
+    updated_at = DateTimeField(null=False, default=lambda: datetime.now(UTC))
+
+    class Meta:
+        database = db
+        table_name = "transactions"
