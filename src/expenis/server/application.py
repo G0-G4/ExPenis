141dc7bdf7d0dc479036d928a577f0fdc6af8dfe
George Grishchenkov <g.grishchenkov@hh.ru>
2026-03-23 20:52:31 +0300

    feat: add crud for mobile app

--- src/expenis/server/application.py
+++ src/expenis/server/application.py
@@ -1,7 +1,7 @@
 import base64
 import io
 from contextlib import asynccontextmanager
-from datetime import date
+from datetime import UTC, date, datetime
 from typing import Annotated
 
 import qrcode
@@ -16,15 +16,19 @@ from .dto import AccountCreateRequest, AccountDto, AccountUpdateRequest, Account
     CurrencyCodes, \
     QRCodeResponse, \
     SessionStatusResponse, Transaction, \
-    TransactionsResponse
+    TransactionCreateRequest, TransactionsResponse
 from ..config import BOT_NAME, COOKIE_DOMAIN, DEV, EXPIRATION_TIME_SECONDS, SECRET
 from ..core.models import Account, Category, Transaction as ModelTransaction, db
 from ..core.service import clear_old_sessions, create_account, create_category, create_default_categories, \
     create_session, \
-    delete_account_by_id_and_user_id, delete_category_by_id_and_user_id, get_category_by_id, \
+    delete_account_by_id_and_user_id, delete_category_by_id_and_user_id, delete_transaction, \
+    delete_transaction_by_id_and_user_id, get_account_by_id, \
+    get_category_by_id, \
     get_session, \
-    get_transactions_for_period, \
-    get_user_account_with_balance, get_user_accounts_with_balance, get_user_categories, update_account, update_category
+    get_transaction_by_id, get_transaction_by_id_and_user_id, get_transactions_for_period, \
+    get_user_account_with_balance, get_user_accounts_with_balance, get_user_categories, save_transaction, \
+    update_account, update_category, update_transaction
+from ..core.service.exchage_rate_service import get_currency_exchange_rate
 from ..core.utils.currency_codes import CODES
 
 
@@ -81,6 +85,54 @@ async def get_user_transactions(
     transactions = await get_transactions_for_period(int(payload.sub), date_from, date_to)
     return TransactionsResponse(transactions=[convert_transaction_to_dto(tx) for tx in transactions])
 
+@app.get("/api/transactions/{transaction_id}")
+async def get_transaction(
+        transaction_id: int,
+        payload: TokenPayload = Depends(auth.access_token_required)
+) -> Transaction:
+    transaction = await get_transaction_by_id_and_user_id(int(payload.sub), transaction_id)
+    return convert_transaction_to_dto(transaction)
+
+@app.post("/api/transactions")
+async def create_transaction_endpoint(
+        transaction_create: TransactionCreateRequest,
+        payload: TokenPayload = Depends(auth.access_token_required)
+) -> Transaction:
+    account = await get_account_by_id(int(payload.sub), transaction_create.account_id)
+    category = await get_category_by_id(int(payload.sub), transaction_create.category_id)
+    transaction = convert_transaction_create_to_model(int(payload.sub), transaction_create, account, category,
+                                                      await get_currency_exchange_rate(account.currency_code))
+    transaction = await save_transaction(transaction)
+    return convert_transaction_to_dto(transaction)
+
+@app.put("/api/transactions/{transaction_id}")
+async def update_transaction_endpoint(
+        transaction_id: int,
+        transaction_create: TransactionCreateRequest,
+        payload: TokenPayload = Depends(auth.access_token_required)
+) -> Transaction:
+    account = await get_account_by_id(int(payload.sub), transaction_create.account_id)
+    category = await get_category_by_id(int(payload.sub), transaction_create.category_id)
+    transaction = await get_transaction_by_id_and_user_id(int(payload.sub), transaction_id)
+    transaction.account = account
+    transaction.category = category
+    transaction.amount = transaction_create.amount
+    transaction.description = transaction_create.description
+    transaction.created_at = transaction_create.created_at
+    transaction.updated_at = datetime.now(UTC)
+    transaction.exchange_rate = await get_currency_exchange_rate(account.currency_code)
+
+    transaction = await update_transaction(transaction)
+    return convert_transaction_to_dto(transaction)
+
+@app.delete("/api/transactions/{transaction_id}")
+async def delete_transaction_endpoint(
+        transaction_id: int,
+        payload: TokenPayload = Depends(auth.access_token_required)
+):
+    await delete_transaction_by_id_and_user_id(int(payload.sub), transaction_id)
+
+
 
 @app.post("/api/create-session")
 async def create_session_route() -> QRCodeResponse:
@@ -120,30 +172,45 @@ async def auth_user(session_id: str, response: Response) -> SessionStatusRespons
 
 
 @app.get("/api/accounts")
-async def get_user_accounts() -> AccountsResponse:
-    accounts = await get_user_accounts_with_balance(433289417)
+async def get_user_accounts(
+        payload: TokenPayload = Depends(auth.access_token_required)
+) -> AccountsResponse:
+    accounts = await get_user_accounts_with_balance(int(payload.sub))
     return AccountsResponse(accounts={acc.id: convert_account_with_balance_to_dto(acc, am) for acc, am in accounts},
                             total=len(accounts))
 
 @app.get("/api/accounts/account/{account_id}")
-async def get_user_accounts(account_id: int) -> AccountDto:
-    account, balance = await get_user_account_with_balance(433289417, account_id)
+async def get_user_accounts(
+        account_id: int,
+        payload: TokenPayload = Depends(auth.access_token_required)
+) -> AccountDto:
+    account, balance = await get_user_account_with_balance(int(payload.sub), account_id)
     return convert_account_with_balance_to_dto(account, balance)
 
 @app.put("/api/accounts/account/{account_id}")
-async def update_account_endpoint(account_id: int, update_request: AccountUpdateRequest) -> AccountDto:
-    account, balance = await get_user_account_with_balance(433289417, account_id)
+async def update_account_endpoint(
+        account_id: int,
+        update_request: AccountUpdateRequest,
+        payload: TokenPayload = Depends(auth.access_token_required)
+) -> AccountDto:
+    account, balance = await get_user_account_with_balance(int(payload.sub), account_id)
     account.name = update_request.name
-    updated_account = await update_account(433289417, account, update_request.amount)
+    updated_account = await update_account(int(payload.sub), account, update_request.amount)
     return convert_account_with_balance_to_dto(updated_account, update_request.amount)
 
 @app.delete("/api/accounts/account/{account_id}")
-async def update_account_endpoint(account_id: int):
-    await delete_account_by_id_and_user_id(433289417, account_id)
+async def update_account_endpoint(
+        account_id: int,
+        payload: TokenPayload = Depends(auth.access_token_required)
+):
+    await delete_account_by_id_and_user_id(int(payload.sub), account_id)
 
 @app.post("/api/accounts/account")
-async def create_account_endpoint(create_request: AccountCreateRequest) -> AccountDto:
-    account = await create_account(433289417, create_request.name, create_request.amount, create_request.currency_code)
+async def create_account_endpoint(
+        create_request: AccountCreateRequest,
+        payload: TokenPayload = Depends(auth.access_token_required)
+) -> AccountDto:
+    account = await create_account(int(payload.sub), create_request.name, create_request.amount, create_request.currency_code)
     return convert_account_with_balance_to_dto(account, create_request.amount)
 
 @app.get("/api/currency/codes")
@@ -155,37 +222,52 @@ async def update_account_endpoint() -> CurrencyCodes:
     )
 
 @app.get("/api/categories")
-async def get_user_categories_endpoint() -> CategoriesResponse:
-    income, expense = await get_user_categories(433289417)
+async def get_user_categories_endpoint(
+        payload: TokenPayload = Depends(auth.access_token_required)
+) -> CategoriesResponse:
+    income, expense = await get_user_categories(int(payload.sub))
     if not income or not expense:
-        await create_default_categories(433289417)
-        income, expense = await get_user_categories(433289417)
+        await create_default_categories(int(payload.sub))
+        income, expense = await get_user_categories(int(payload.sub))
     categories = {category.id : convert_category_to_dto(category) for category in income + expense}
     return CategoriesResponse(
         categories=categories
     )
 
 @app.get("/api/categories/{category_id}")
-async def get_user_category_endpoint(category_id: int) -> CategoryDto:
-    category = await get_category_by_id(433289417, category_id)
+async def get_user_category_endpoint(
+        category_id: int,
+        payload: TokenPayload = Depends(auth.access_token_required)
+) -> CategoryDto:
+    category = await get_category_by_id(int(payload.sub), category_id)
     return convert_category_to_dto(category)
 
 @app.post("/api/categories")
-async def create_category_endpoint(create_request: CategoryCreateRequest) -> CategoryDto:
-    category = await create_category(433289417, create_request.name, create_request.type)
+async def create_category_endpoint(
+        create_request: CategoryCreateRequest,
+        payload: TokenPayload = Depends(auth.access_token_required)
+) -> CategoryDto:
+    category = await create_category(int(payload.sub), create_request.name, create_request.type)
     return convert_category_to_dto(category)
 
 @app.put("/api/categories/{category_id}")
-async def create_category_endpoint(category_id: int, create_request: CategoryCreateRequest) -> CategoryDto:
-    category = await get_category_by_id(433289417, category_id)
+async def create_category_endpoint(
+        category_id: int,
+        create_request: CategoryCreateRequest,
+        payload: TokenPayload = Depends(auth.access_token_required)
+) -> CategoryDto:
+    category = await get_category_by_id(int(payload.sub), category_id)
     category.name = create_request.name
     category.type = create_request.type
     category = await update_category(category)
     return convert_category_to_dto(category)
 
 @app.delete("/api/categories/{category_id}")
-async def create_category_endpoint(category_id: int):
-    await delete_category_by_id_and_user_id(433289417, category_id)
+async def create_category_endpoint(
+        category_id: int,
+        payload: TokenPayload = Depends(auth.access_token_required)
+):
+    await delete_category_by_id_and_user_id(int(payload.sub), category_id)
 
 def convert_account_with_balance_to_dto(account: Account, balance: float):
     return AccountDto(
@@ -201,8 +283,10 @@ def convert_transaction_to_dto(transaction: ModelTransaction) -> Transaction:
     return Transaction(
         id=transaction.id,
         account=transaction.account.name,
+        account_id=transaction.account.id,
         type=transaction.category.type,
         category=transaction.category.name,
+        category_id=transaction.category.id,
         amount=transaction.amount * transaction.exchange_rate,
         description=transaction.description
     )
@@ -212,4 +296,18 @@ def convert_category_to_dto(category: Category) -> CategoryDto:
         id=category.id,
         name=category.name,
         type=category.type
+    )
+
+
+def convert_transaction_create_to_model(user_id: int, transaction_create: TransactionCreateRequest, account: Account,
+                                        category: Category, exchange_rate: float) -> ModelTransaction:
+    return ModelTransaction(
+        user_id=user_id,
+        account=account,
+        category=category,
+        amount=transaction_create.amount,
+        description=transaction_create.description,
+        created_at=datetime.now(UTC) if transaction_create.created_at is None else transaction_create.created_at,
+        updated_at=datetime.now(UTC),
+        exchange_rate=exchange_rate
     )
