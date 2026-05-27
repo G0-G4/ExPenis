2c654fb5f1eafccd609ab0abd2795299bb4b5303
George Grishchenkov <g.grishchenkov@hh.ru>
2026-05-28 00:14:26 +0300

    chore: disable hot reloading in prod

--- src/expenis/server/__main__.py
+++ src/expenis/server/__main__.py
@@ -1,11 +1,16 @@
 import uvicorn
 
+from ..config import DEV
 from ..core.logging_config import setup_logging
 
 
 def main():
     log_config = setup_logging()
-    uvicorn.run("src.expenis.server.application:app", host="0.0.0.0", port=8000, reload=True, log_config=log_config)
+    options = {"host": "0.0.0.0", "port": 8000, "log_config": log_config}
+    if DEV:
+        options["reload"] = True
+        options["reload_excludes"] = ["logs/"]
+    uvicorn.run("src.expenis.server.application:app", **options)
 
 if __name__ == "__main__":
     main()
